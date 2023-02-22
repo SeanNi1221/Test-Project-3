420b59cb597f8554b2508cfcf517eddf9eb20268
SeanNi1221 <sean.ni.21@outlook.com>
2023-02-22 11:29:51 +0800

    Simplify codes

--- Assets/Scripts/Zoom & Pan/CameraController.cs
+++ Assets/Scripts/Zoom & Pan/CameraController.cs
@@ -7,10 +7,10 @@ public class CameraController : MonoBehaviour
     //To record camera state at a specific moment
     private class CameraState {
         public readonly float Fov;
-        public readonly float viewSize;
+        public readonly float ViewSize;
         public CameraState(Camera camera) {
             Fov = camera.fieldOfView;
-            viewSize = Mathf.Tan(Mathf.Deg2Rad * Fov/2);
+            ViewSize = Mathf.Tan(Mathf.Deg2Rad * Fov/2);
         }
     }
     //To constrain pan and zoom inside a rect area
@@ -31,7 +31,7 @@ public class CameraController : MonoBehaviour
             var currentViewSize = Mathf.Tan(
                 Mathf.Deg2Rad * _controller._camera.fieldOfView/2
             );
-            Scale = _controller._origin.viewSize/currentViewSize;
+            Scale = _controller._origin.ViewSize/currentViewSize;
             var expand = Scale - 1;
             XMin = YMin = -expand;
             XMax = YMax = expand;
@@ -40,21 +40,21 @@ public class CameraController : MonoBehaviour
     private static class InputProcessor {
         public static readonly float ScrollWheel = Time.deltaTime *
 # if UNITY_WEBGL
-        500;
+            500;
 # else
-        500;
+            500;
 #endif
         public static readonly float MouseX = Time.deltaTime *
 # if UNITY_WEBGL
-        1;
+            1;
 # else
-        5;
+            5;
 #endif
         public static readonly float MouseY = Time.deltaTime *
 # if UNITY_WEBGL
-        1;
+            1;
 # else
-        5;
+            5;
 #endif
     }
     [Range(0,10)] public float ZoomSpeed = 1;
@@ -64,14 +64,15 @@ public class CameraController : MonoBehaviour
     public bool IsPanning => _panDelta != Vector2.zero;
     private float _zoomDelta;
     private Vector2 _panDelta;
-    //The state when there's no pan, no zoom applied
-    private CameraState _origin;
-    private CameraController.Canvas _canvas;
-    //Has the camera been touched during the current frame?
+
+    //Has the camera been touched at the current frame?
     private bool _isDirty;
     private float _lensShiftX;
     private float _lensShiftY;
     [SerializeField] private Camera _camera;
+    //The state when there's no pan, no zoom applied
+    private CameraState _origin;
+    private CameraController.Canvas _canvas;
     [SerializeField] private Text _debugInfo;
     void OnValidate() {
         if (!_camera) _camera = Camera.main;
@@ -103,36 +104,29 @@ public class CameraController : MonoBehaviour
     internal void Zoom(float delta) {
         if (delta < 0 && _camera.fieldOfView >= _origin.Fov) return;
 
-        float newFov = _camera.fieldOfView - delta;
-        _camera.fieldOfView = Mathf.Clamp(newFov, 1, _origin.Fov);
+        _camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView - delta, 1, _origin.Fov);
 
-        //The scale of the canvas changes with fov, therefore a compensation
-        //lens shift is needed after changing the fov to ensure the view center
-        //is not changed.
+        //The scale of the view changes with fov, therefore a compensation
+        //lens shift is needed to ensure that the view center stays unmoved.
         float previousScale = _canvas.Scale;
         _canvas.UpdateScale();
         float compensation = _canvas.Scale/previousScale;
 
-        _lensShiftX *= compensation;
-        _lensShiftY *= compensation;
-
-        _lensShiftX = Mathf.Clamp(_lensShiftX, _canvas.XMin, _canvas.XMax);
-        _lensShiftY = Mathf.Clamp(_lensShiftY, _canvas.YMin, _canvas.YMax);
+        _lensShiftX = Mathf.Clamp(_lensShiftX * compensation, _canvas.XMin, _canvas.XMax);
+        _lensShiftY = Mathf.Clamp(_lensShiftY * compensation, _canvas.YMin, _canvas.YMax);
 
         _isDirty = true;
     }
     internal void Pan(Vector2 delta) {
-        float newShiftX = _lensShiftX - delta.x;
-        _lensShiftX = Mathf.Clamp(newShiftX, _canvas.XMin, _canvas.XMax);
-
-        float newShiftY = _lensShiftY - delta.y;
-        _lensShiftY = Mathf.Clamp(newShiftY, _canvas.YMin, _canvas.YMax);
+        _lensShiftX = Mathf.Clamp(_lensShiftX - delta.x, _canvas.XMin, _canvas.XMax);
+        _lensShiftY = Mathf.Clamp(_lensShiftY - delta.y, _canvas.YMin, _canvas.YMax);
 
         _isDirty = true;
     }
     private void GetInputs() {
         var zoomInput = Input.GetAxis("Mouse ScrollWheel");
-        _zoomDelta = zoomInput == 0 ? 0 : zoomInput * ZoomSpeed * InputProcessor.ScrollWheel;
+        _zoomDelta = zoomInput ==
+            0 ? 0 : zoomInput * ZoomSpeed * InputProcessor.ScrollWheel;
 
         var panInput = Input.GetButton("Fire1") ?
             new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) :
